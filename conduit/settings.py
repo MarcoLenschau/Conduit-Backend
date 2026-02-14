ef17ada2cd00319b502c1f34121492420725d903
MarcoLenschau <contact@marco-lenschau.de>
2026-02-14 21:13:37 +0100

    chore: Load env variable from env file and system variable

--- conduit/settings.py
+++ conduit/settings.py
@@ -13,21 +13,21 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DEBUG"))
-
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(',')
+if os.path.exists(".env"):
+    load_dotenv()
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DEBUG = bool(os.getenv("DEBUG"))
+    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(',')
+else:
+    SECRET_KEY = os.environ.get("SECRET_KEY", '2^f+3@v7$v1f8yt0!s)3-1t$)tlp+xm17=*g))_xoi&&9m#2a&')
+    DEBUG = bool(os.environ.get("DEBUG", "False"))
+    ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(',')
 
 # Application definition
 
@@ -83,15 +83,28 @@ WSGI_APPLICATION = 'conduit.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.path.exists(".env"):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv("POSTGRES_DB"),
+            'USER': os.getenv("POSTGRES_USER"),
+            'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+            'HOST': os.getenv("POSTGRES_HOST"),
+            'PORT': os.getenv("POSTGRES_PORT"),
+        }
     }
-}
-
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get("POSTGRES_DB", "postgres"),
+            'USER': os.environ.get("POSTGRES_USER", "postgres"),
+            'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "postgres"),
+            'HOST': os.environ.get("POSTGRES_HOST", "localhost"),
+            'PORT': os.environ.get("POSTGRES_PORT", "5432"),
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
